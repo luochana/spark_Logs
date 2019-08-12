7ce3c9e75d1ebadc3fbf49dbf50a6c59ee5c3aa2
luochana <1329127421@qq.com>
2019-08-12 18:31:30 +0800

    add something new

--- spark_project/src/main/scala/Streaming/kafkaConsumer.scala
+++ spark_project/src/main/scala/Streaming/kafkaConsumer.scala
@@ -1,11 +1,14 @@
 package Streaming
 
+import java.util.Date
+
 import domain.CheckLog
 import kafka.serializer.StringDecoder
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.kafka.KafkaUtils
 import utils.DateUtils
+import org.apache.phoenix.spark._
 
 object kafkaConsumer {
   def main(args:Array[String]):Unit={
@@ -17,7 +20,6 @@ object kafkaConsumer {
       "serializer.class" -> "kafka.serializer.StringDecoder")
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](
       ssc, kafkaParams, topicsSet)
-
 
     val LogInfo=messages.map(_._2).map(lines=>{
       val infos=lines.split("\t")
@@ -36,7 +38,50 @@ object kafkaConsumer {
     LogInfo.print()
 
 
-    
+    val listLogInfo=List(messages.map(_._2).map(lines=>{
+      val infos=lines.split("\t")
+      val ip=infos(0)
+      val time=DateUtils.parseTime(infos(1))
+      var courseId=0
+      val url=infos(2)
+      CheckLog(ip,time,courseId,infos(3).toInt,infos(4))
+    }))
+
+    ssc.sparkContext.parallelize(Seq(listLogInfo)).saveToPhoenix("LogInfo",Seq("ip","time","courseId","statusCode","searchUrl"),zkUrl = Some("localhost:2181"))
+
+
+    val currentTime=new Date()
+    val listCourseCount= List(LogInfo.map(x => {
+      //将CheckLog格式的数据转为20180724_courseId+currentTime
+      //rowkey加上currentTime是为了防止不同批次被处理的数据不会产生覆盖的现象,因为20秒处理一次
+      (x.time.substring(0, 8) + "_" + x.courseId+ "_"+currentTime, 1)
+    }).reduceByKey(_ + _))
+
+    ssc.sparkContext.parallelize(Seq(listCourseCount)).saveToPhoenix("CourseCount", Seq("ID", "COUNT"), zkUrl = Some("localhost:2181"))
+
+
+    val listSearchCount=List(LogInfo.map(x=>{
+      /**
+        * https://www.sogou.com/web?query=
+        * =>https:/www.sogou.com/web?query=
+        */
+      val url=x.referes.replaceAll("//","/")
+      val splits=url.split("/")
+      var search=""
+      if(splits.length>=2){
+        search=splits(1)
+      }
+      (x.time.substring(0,8),x.courseId,search)
+    }).filter(x=>x._3!="").map(info=>{
+      val day_search_course=info._1+"_"+info._3+"_"+info._2
+      (day_search_course+"_"+currentTime,1)
+    }).reduceByKey(_+_))
+
+    ssc.sparkContext.parallelize(Seq(listSearchCount)).saveToPhoenix("SearchCount", Seq("ID", "COUNT"), zkUrl = Some("localhost:2181"))
+
+
+    ssc.start()
+    ssc.awaitTermination()
   }
 
 }
---
+++ spark_project/src/main/scala/test.scala
@@ -0,0 +1,8 @@
+import java.util.Date
+
+object test {
+  def main(args:Array[String]):Unit={
+    println(new Date())
+  }
+
+}
