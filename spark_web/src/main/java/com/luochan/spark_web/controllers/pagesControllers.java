927778d021108a3a173aaf7c7c6bf21fce7eb8f3
luochana <1329127421@qq.com>
2019-08-14 14:21:24 +0800

    add something new

--- spark_web/src/main/java/com/luochan/spark_web/controllers/pagesControllers.java
+++ spark_web/src/main/java/com/luochan/spark_web/controllers/pagesControllers.java
@@ -4,11 +4,33 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 @Controller
 @EnableAutoConfiguration
 public class pagesControllers {
     @RequestMapping("index")
     public String test() {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+            connection = DriverManager.getConnection("jdbc:phoenix:localhost:2181");
+            statement = connection.createStatement();
+            statement.execute("upsert into test1 values (3, 'note of huhong')");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
        return "redirect:/index.html";
     }
 
